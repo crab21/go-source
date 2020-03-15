a217dadeb4ad3e484e856c023e6e73c4d4e0bd45
imrcrab <imrcrab@163.com>
2020-03-15 20:29:13 +0800

    添加注释

--- gosource/src/com.py/sourcego/source_slice.go
+++ gosource/src/com.py/sourcego/source_slice.go
@@ -1,13 +1,19 @@
 package sourcego
 
+// []byte复制
 
+//赋值方式
 func SliceCopyOfNormal(des, source []string) {
 	des = source
 }
+//copy方式   可以剔除原有的引用，产生一个新的[]byte
 func SliceCopyOfApi(des,source []string) {
 	copy(des,source)
 }
 
+//slice append方式
+
+//make后再添加
 func SliceAppendMake(source []string) {
 	a:=make([]string,len(source),len(source))
 
@@ -15,6 +21,7 @@ func SliceAppendMake(source []string) {
 
 }
 
+//直接添加 √
 func SliceAppendNormal(source []string) {
 	var b []string
 	source = append(b,source...)
