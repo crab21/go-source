a1f73bc2c278ab4206da8d5f38085958688dca1b
crab <imrcrab@163.com>
2020-04-30 22:02:10 +0800

    [slice] 0.0.17 add : answer the issues 4

--- gosource/src/com.py/sourcego/sourceruntime/source_alg_test.go
+++ gosource/src/com.py/sourcego/sourceruntime/source_alg_test.go
@@ -2,6 +2,11 @@ package sourceruntime
 
 import "testing"
 
+/**
+compare slice []interface{}
+
+that compare the first element,according the first element type.
+ */
 func TestAlgMemequals(t *testing.T) {
 	AlgMemequals()
 }
